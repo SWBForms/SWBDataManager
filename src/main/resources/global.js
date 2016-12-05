9bc825d067dc058b3986823edac4e1e1093bdfda
Javier Solis <softjei@gmail.com>
2016-12-05 12:51:45 -0600

    Dependence resolve

---
+++ src/main/resources/global.js
@@ -0,0 +1,20 @@
+
+
+var eng = {    
+    dataStores:{},                      //DataStores
+    dataSources: {},                    //Datasources
+    fieldProcesors:{},                  //Procesadores de field elements
+    validators:{},                      //Validator templates
+    dataServices:{},                    //Servicios
+    dataProcessors:{},                  //DataProcessors
+    dataExtractors:{},                  //DataExtractors
+    fileSources: {},
+    routes:{},
+};
+
+
+
+
+
+
+
